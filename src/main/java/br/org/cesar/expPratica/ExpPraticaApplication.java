888629055975444379275144e05556cf4e03f7c0
Beatriz Brandão <beatriz_cb916@hotmail.com>
2022-11-21 10:22:42 -0300

    Adicionar projeto inicial de SpringBoot

---
+++ src/main/java/br/org/cesar/expPratica/ExpPraticaApplication.java
@@ -0,0 +1,13 @@
+package br.org.cesar.expPratica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpPraticaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpPraticaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/org/cesar/expPratica/ExpPraticaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.org.cesar.expPratica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpPraticaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
