d72053c52f15cb98d5c08a78c5ed583916fc18ea
beatriz <beatrizg.m@yahoo.com>
2022-11-28 11:55:31 -0300

    Armazenar um documento de parecer técnico

--- src/main/java/br/org/cesar/expPratica/TechnicalReportController.java
+++ src/main/java/br/org/cesar/expPratica/TechnicalReportController.java
@@ -3,6 +3,7 @@ package br.org.cesar.expPratica;
 import java.io.IOException;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,10 +11,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TechnicalReportController {
 
+    @Autowired
+    private TechnicalReportRepository repository;
+
     @PostMapping("/report")
     public String insertTechnicalReport (@Validated @RequestBody(required=true) TechnicalReport technicalReport)
             throws InvalidFormatException, IOException{
         XlsxConverter.generateXlsx(technicalReport);
+        TechnicalReport result = repository.save(technicalReport);
         return "Cadastro realizado com sucesso! "+technicalReport;
     }
 
