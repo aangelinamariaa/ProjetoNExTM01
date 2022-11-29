30aefad9d4baa205c5655718bbf52bc33042efe8
Beatriz Maciel <111383519+beatrizg-m@users.noreply.github.com>
2022-11-29 11:36:07 -0300

    Armazenar um documento de parecer técnico (#7)

--- src/main/java/br/org/cesar/expPratica/TechnicalReport.java
+++ src/main/java/br/org/cesar/expPratica/TechnicalReport.java
@@ -3,6 +3,8 @@ package br.org.cesar.expPratica;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
