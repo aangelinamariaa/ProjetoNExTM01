26d3dab51bb577a90c6e68b54ad44f6eb21198b2
Beatriz Brandão <117690786+biacbrandao@users.noreply.github.com>
2022-11-30 12:58:11 -0300

    Retornar o documento do parecer técnico que foi armazenado na API de cadastrar parecer técnico (#8)

--- src/main/java/br/org/cesar/expPratica/DocumentController.java
+++ src/main/java/br/org/cesar/expPratica/DocumentController.java
@@ -1,14 +1,24 @@
 package br.org.cesar.expPratica;
 
-//import org.springframework.http.*;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/document")
 public class DocumentController {
+	
+    @Autowired
+    private TechnicalReportRepository repository;
 
     @GetMapping("/{ticketId}")
     public String getDocument(@PathVariable (value = "ticketId") long id){
-        return "API para geração automática de Parecer Técnico";
+    	Optional<TechnicalReport> technicalReport = repository.findById(id);
+    	if(technicalReport.isPresent()) {
+    		return "Documento  encontrado";
+    	}
+        return "Sem documento";
     }
+    
 }
