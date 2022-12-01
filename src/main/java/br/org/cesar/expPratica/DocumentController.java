92874f209e7cc4db1a646639b7509c3de462b7cc
beatriz <beatrizg.m@yahoo.com>
2022-11-30 21:15:53 -0300

    Validar os campos de entrada no fluxo de obter um documento do parecer técnico

--- src/main/java/br/org/cesar/expPratica/DocumentController.java
+++ src/main/java/br/org/cesar/expPratica/DocumentController.java
@@ -1,13 +1,16 @@
 package br.org.cesar.expPratica;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
-//import org.springframework.http.*;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/document")
@@ -15,14 +18,20 @@ public class DocumentController {
 	
     @Autowired
     private TechnicalReportRepository repository;
+    private String ticketIdRegex = "^([0-9]{17})\\z";
+
 
     @GetMapping("/{ticketId}")
-    public String getDocument(@PathVariable (value = "ticketId") long id){
-    	Optional<TechnicalReport> technicalReport = repository.findById(id);
+    public ResponseEntity getDocument(@PathVariable (value = "ticketId") String id){
+        if (!id.matches(ticketIdRegex)) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Entrada invalida");
+        }
+        Long longId = Long.parseLong(id);
+        Optional<TechnicalReport> technicalReport = repository.findById(longId);
     	if(technicalReport.isPresent()) {
-    		return "Documento  encontrado";
+    		return ResponseEntity.ok("Documento encontrado");
     	}
-        return "Sem documento";
+    	return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Sem documento");
     }
 
     @GetMapping("/get/{ticketId}")
